e5d96dda214f9b4f5d782d8e18dc0837e4e0f0ea
Ray Mulligan <rmullig2@optonline.net>
2016-12-06 18:11:48 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,23 @@
+require 'pry'
 class FiguresController < ApplicationController
 
   get '/figures/new' do
     erb :'/figures/new'
+  end
+
+  post '/figures' do
+    #binding.pry
+    @figure = Figure.find_or_create_by(:name => params[:figure][:name])
+    @titles = params[:figure][:title_ids]
+    @titles.each do |title|
+      @figure.titles << Title.find(title)
+    end
+    binding.pry
+    @landmarks = params[:figure][:landmark_ids]
+    @landmarks.each do |landmark|
+      @figure.landmarks << Landmark.find(landmark)
+    end
+    @figure.save
   end
 
 end
